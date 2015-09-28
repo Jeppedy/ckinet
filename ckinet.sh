e3e116fd6dedbce77e16e25f67a0bdbda9bbcdf8
Jeffrey Herr <jeff@theherrs.com>
2015-09-28 13:41:49 -0500

    Initial Version

---
+++ ckinet.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LOGFILE=/home/jherr/projects/ckinet/ckinet.log
+
+COUNTER=0
+while [ $COUNTER -lt 5 ]; do
+  if ping -c1 -q 8.8.8.8 &> /dev/null; then 
+    echo "Internet accessible.  Continuing."
+    exit 0
+  else
+    echo "Ping problem.  Retrying." >> $LOGFILE
+    date >> $LOGFILE
+
+    let COUNTER=COUNTER+1
+    sleep 30
+  fi
+done
+
+echo "Internet not accessible.  Resetting..." >> $LOGFILE
+date >> $LOGFILE
+
+sudo /sbin/shutdown -r now
+
