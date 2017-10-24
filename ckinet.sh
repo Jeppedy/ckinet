ce3c3f69b0b087add88640c3d358fd9a9c45fd50
Jeffrey Herr <jeff@theherrs.com>
2017-10-23 21:54:42 -0500

    Added time variables

--- ckinet.sh
+++ ckinet.sh
@@ -2,21 +2,24 @@
 
 LOGFILE=/var/log/ckinet.log
 
+MAXRETRIES=10
+SLEEPTIMESECS=60
 COUNTER=0
-while [ $COUNTER -lt 5 ]; do
+while [ $COUNTER -lt $MAXRETRIES ]; do
   if ping -c1 -q 8.8.8.8 &> /dev/null; then 
     echo "Internet accessible.  Continuing."
     exit 0
   else
-    echo "Ping problem.  Retrying." >> $LOGFILE
+    echo "Ping problem [$COUNTER].  Retrying." >> $LOGFILE
+    echo "Ping problem [$COUNTER].  Retrying."
     date >> $LOGFILE
 
     let COUNTER=COUNTER+1
-    sleep 30
+    sleep $SLEEPTIMESECS
   fi
 done
 
-echo "Internet not accessible.  Resetting..." >> $LOGFILE
+echo "Internet not accessible for $MAXRETRIES tries at $SLEEPTIMESECS sec each.  Rebooting..." >> $LOGFILE
 date >> $LOGFILE
 
 sudo /sbin/shutdown -r now
